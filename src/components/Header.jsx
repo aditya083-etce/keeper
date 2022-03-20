6121fad9f89f1651c5b706e4a14a5e1dc09f1efc
aditya083-etce <you@example.com>
2022-03-20 22:47:17 +0530

    done

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BorderColorSharpIcon from '@mui/icons-material/BorderColorSharp';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BorderColorSharpIcon /> 
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
